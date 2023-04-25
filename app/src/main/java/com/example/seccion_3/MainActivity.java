176d6903648ca224a57d727b43ab427ed62fd932
jreyes1990 <jarl1990mt@gmail.com>
2023-04-24 23:01:03 -0600

    Uso de Recycler View - Adapter - Parte 3.

--- app/src/main/java/com/example/seccion_3/MainActivity.java
+++ app/src/main/java/com/example/seccion_3/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.seccion_3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+  private List<String> names;
+  private RecyclerView mRecyclerView;
+  private RecyclerView.Adapter mAdapter;
+  private RecyclerView.LayoutManager mLayoutManager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    names = this.getAllNames();
+
+    mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    mLayoutManager = new LinearLayoutManager(this);
+    mAdapter = new MyAdapter(names, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {
+      @Override
+      public void onItemClick(String name, int position) {
+        Toast.makeText(MainActivity.this, name+" - "+position, Toast.LENGTH_SHORT).show();
+      }
+    });
+
+    mRecyclerView.setLayoutManager(mLayoutManager);
+    mRecyclerView.setAdapter(mAdapter);
+  }
+
+  private List<String> getAllNames(){
+    return new ArrayList<String>(){
+      {
+        add("Alejandro");
+        add("Jose");
+        add("Barrera");
+        add("Ruben");
+        add("Antonio");
+      }
+    };
   }
 }
--- app/src/main/java/com/example/seccion_3/MyAdapter.java
+++ app/src/main/java/com/example/seccion_3/MyAdapter.java
@@ -1,11 +1,12 @@
 package com.example.seccion_3;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
@@ -20,10 +21,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     this.itemClickListener = itemClickListener;
   }
 
-  @NonNull
   @androidx.annotation.NonNull
   @Override
-  public ViewHolder onCreateViewHolder(@NonNull @androidx.annotation.NonNull ViewGroup viewGroup, int i) {
+  public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
     View v = LayoutInflater.from(viewGroup.getContext()).inflate(layout, viewGroup, false);
     ViewHolder vh = new ViewHolder(v);
 
@@ -31,7 +31,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
   }
 
   @Override
-  public void onBindViewHolder(@NonNull @androidx.annotation.NonNull ViewHolder holder, int position) {
+  public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
     holder.bind(names.get(position), itemClickListener);
   }
 
@@ -43,7 +43,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
   public static class ViewHolder extends RecyclerView.ViewHolder {
     public TextView textViewName;
 
-    public ViewHolder(@NonNull @androidx.annotation.NonNull View itemView) {
+    public ViewHolder(@NonNull View itemView) {
       super(itemView);
       this.textViewName = (TextView) itemView.findViewById(R.id.textViewName);
     }
@@ -54,7 +54,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
       itemView.setOnClickListener(new View.OnClickListener(){
         @Override
         public void onClick(View view){
-          listener.onItemClick(names, getAdapterPosition());
+          listener.onItemClick(names, getBindingAdapterPosition());
         }
       });
     }
