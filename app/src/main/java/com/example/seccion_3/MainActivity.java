3417148dda5f6380c2ebe3e8d451afde1b374ee8
jreyes1990 <jarl1990mt@gmail.com>
2023-04-24 23:31:18 -0600

    Uso de Recycler View - Añadir y Borrar.

--- app/src/main/java/com/example/seccion_3/MainActivity.java
+++ app/src/main/java/com/example/seccion_3/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.seccion_3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
   private RecyclerView mRecyclerView;
   private RecyclerView.Adapter mAdapter;
   private RecyclerView.LayoutManager mLayoutManager;
+  private int counter = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +33,45 @@ public class MainActivity extends AppCompatActivity {
     mAdapter = new MyAdapter(names, R.layout.recycler_view_item, new MyAdapter.OnItemClickListener() {
       @Override
       public void onItemClick(String name, int position) {
-        Toast.makeText(MainActivity.this, name+" - "+position, Toast.LENGTH_SHORT).show();
+        deleteName(position);
+        // Toast.makeText(MainActivity.this, name+" - "+position, Toast.LENGTH_SHORT).show();
       }
     });
 
+    mRecyclerView.setHasFixedSize(true);
+    mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
     mRecyclerView.setLayoutManager(mLayoutManager);
     mRecyclerView.setAdapter(mAdapter);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu, menu);
+
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()){
+      case R.id.add_name:
+        this.addName(0);
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  private void addName(int position){
+    names.add(position, "New Name "+(++counter));
+    mAdapter.notifyItemInserted(position);
+    mLayoutManager.scrollToPosition(position);
+  }
+
+  private void deleteName(int position){
+    names.remove(position);
+    mAdapter.notifyItemRemoved(position);
   }
 
   private List<String> getAllNames(){
@@ -47,4 +85,5 @@ public class MainActivity extends AppCompatActivity {
       }
     };
   }
+
 }
